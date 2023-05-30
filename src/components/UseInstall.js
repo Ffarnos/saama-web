8f6b75ae88074099370030d355fc862944d4c2c9
Ffarnos <ffarnos12@gmail.com>
2023-05-30 19:22:06 -0300

    + Install Button

---
+++ src/components/UseInstall.js
@@ -0,0 +1,48 @@
+// noinspection JSUnresolvedVariable
+import {useEffect, useState} from 'react';
+
+let deferredPrompt = undefined;
+
+export const useConfigureInstall = () => {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+    });
+  }
+};
+
+const useInstall = () => {
+  const [, update] = useState();
+  useEffect(() => {
+    // Este if es para evitar un error con el SSR, ya que window es 'undefined' y peta el CI
+    if (typeof window !== 'undefined') {
+      // Instala el listener para el A2HS
+      let listener;
+      listener = window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        // Guardalo en un Hook para asi re-renderizar el componente ahora que ya esta disponible
+        deferredPrompt = e;
+        update(true);
+        return () => {
+          window.removeEventListener('beforeinstallprompt', listener);
+        };
+      });
+    }
+  }, []);
+  return [
+    deferredPrompt !== undefined,
+    () => {
+      if (deferredPrompt != null) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            deferredPrompt = null;
+            update(undefined);
+          }
+        });
+      }
+    }];
+};
+
+export default useInstall;
