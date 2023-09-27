9849329280559ebc9c638af9807ecd4e72a82f24
Ffarnos <ffarnos12@gmail.com>
2023-09-27 19:23:22 -0300

    Fixes

--- src/pages/intro-text.js
+++ src/pages/intro-text.js
@@ -34,7 +34,9 @@ const IntroText = () => {
             document.removeEventListener('keydown', handleKeyDown);
         };
     }, [number]);
-    return <LoginCheck>TextComponent(number, setNumber)</LoginCheck>
+    return <LoginCheck>
+        {TextComponent(number, setNumber)}
+    </LoginCheck>
 }
 
 const Navigate = ({number, setNumber}) => <Container>
