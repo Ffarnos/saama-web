6c0573d13bc2584b0d1e8cb8bc58f82c8b103fde
Ffarnos <ffarnos12@gmail.com>
2023-09-27 17:34:23 -0300

    Change firebaseConfig with new google account

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
+
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
       `La aplicacion se ha actualizado ` +
@@ -13,14 +14,14 @@ export const onServiceWorkerUpdateReady = () => {
 }
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvF1yC5lmZ9YLaWEPHqZfHVZHBg1ChAPQ",
-  authDomain: "saama-4bc7b.firebaseapp.com",
-  projectId: "saama-4bc7b",
-  storageBucket: "saama-4bc7b.appspot.com",
-  messagingSenderId: "177528645527",
-  appId: "1:177528645527:web:6127415a2b13d2b99706d2",
-  measurementId: "G-JZCM5YZV7H",
-  databaseURL: "https://saama-4bc7b-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyAEwgR9D1dyYHeAn2BQryHm-IuipfgBCrs",
+  authDomain: "terapia-genesis.firebaseapp.com",
+  projectId: "terapia-genesis",
+  storageBucket: "terapia-genesis.appspot.com",
+  messagingSenderId: "937946542554",
+  appId: "1:937946542554:web:98a501ed67031108f490e3",
+  measurementId: "G-SG8Q78JFK7",
+  databaseURL: "https://terapia-genesis-default-rtdb.firebaseio.com/",
 };
 
 export const app = initializeApp(firebaseConfig);
