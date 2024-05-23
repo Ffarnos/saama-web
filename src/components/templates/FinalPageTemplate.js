64513d3ce9068f97fe77737630bd34f41f88498a
Ffarnos <ffarnos12@gmail.com>
2024-05-23 16:34:30 -0300

    + Keys for History

--- src/components/templates/FinalPageTemplate.js
+++ src/components/templates/FinalPageTemplate.js
@@ -6,20 +6,38 @@ import styled from "styled-components";
 import ResponsiveImage from "../apis/ResponsiveImage";
 import {Background} from "../Commons";
 import LoginCheck from "../login/LoginCheck";
+import createAndSendPDF from "../apis/pdf-email";
+import {TextField} from "@mui/material";
 
 
 const FinalPageTemplate = ({ pageContext }) => {
     useEffect(() => {
         const handleKeyDown = (event) => {
-            if (event.key === 'Enter')
+            if (event.altKey) {
+                let history = localStorage.getItem("history");
+
+                if (!history) history = [];
+                else history = JSON.parse(history);
+                if (event.key === 'r') {
+                    history.push("ramificar");
+                } else if (event.key === 'b') {
+                    history.remove(history.length-1);
+                    navigate(-1)
+                }
+
+                localStorage.setItem("history", JSON.stringify(history));
+            }
+            else if (event.key === 'Enter')
                 navigate("/circulo-base")
-            else if (event.key === 'Backspace')
+            else if (event.ctrlKey && event.key === 'Backspace')
                 navigate("/")
             else if (event.key === 'ArrowLeft')
                 navigate(-1)
             else if (event.key === 'ArrowRight')
                 navigate(+1)
-            else if (event.key === 'Spacebar')
+            else if (event.key === 'Control') {
+                createAndSendPDF().then(r => console.log("PDF CREADO CORRECTAMENTE"))
+            }else if (event.key === 'Spacebar')
                 navigate("/intro-text")
         };
 
@@ -38,11 +56,13 @@ const FinalPageTemplate = ({ pageContext }) => {
     let textComponents;
     if (separation) {
         textSeparate = desc.split(". ");
-        textComponents = textSeparate.map((sentence, index) => (
-            <ResponsiveText key={index} scale={0.5} color={"white"}>
-                {sentence.replace(".", "")}
-            </ResponsiveText>
-        ));
+        textComponents = textSeparate.map((sentence, index) => {
+            return <div>
+                <ResponsiveText key={index} scale={0.5} color={"white"}>
+                    {sentence.replace(".", "")}
+                </ResponsiveText>
+            </div>
+        });
     }
     return <LoginCheck>
         <Background style={{backgroundImage: `url(${imagePath})`}}>
@@ -52,6 +72,37 @@ const FinalPageTemplate = ({ pageContext }) => {
                 {separation ? textComponents
                     : <Text scale={0.5} color={"white"}>{desc}</Text>}
 
+                {separation && <div>
+                    <TextField id="emocion" label="Emocion" variant="filled" margin="normal"
+                               onChange={(e) => {
+                                   let emotions = localStorage.getItem("history");
+
+                                   if (!emotions)
+                                       emotions = [];
+                                   else
+                                       emotions = JSON.parse(emotions);
+
+                                   const lastEmotion = emotions[emotions.length - 1];
+                                   const lastEmotionLink = lastEmotion.split(", ")[0];
+
+                                   emotions[emotions.length - 1] = lastEmotionLink + ", " + e.target.value;
+
+                                   localStorage.setItem("history", JSON.stringify(emotions));
+                               }}
+                               sx={{
+                                   backgroundColor: 'white',
+                                   '&:hover': {
+                                       backgroundColor: 'white',
+                                   },
+                                   '&.Mui-focused': {
+                                       backgroundColor: 'white',
+                                   },
+                                   '& .MuiFilledInput-root': {
+                                       backgroundColor: 'white'
+                                   }
+                               }}
+                    />
+                </div>}
                 {imageBody && <BodyImage src={imageBodyPath} scale={4}/>}
                 <Container>
                     <NavigationButtonsInLine/>
