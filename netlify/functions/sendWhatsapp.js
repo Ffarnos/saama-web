98ab3f3dcf03dc3db6477729d2c4c115628cb94e
Ffarnos <ffarnos12@gmail.com>
2024-05-24 17:40:29 -0300

    fixing errors

--- netlify/functions/sendWhatsapp.js
+++ netlify/functions/sendWhatsapp.js
@@ -15,22 +15,25 @@ exports.handler = async (event, context) => {
         const pdfBase64 = body.pdf;
         const pdfBuffer = Buffer.from(pdfBase64, 'base64');
 
-        // Configura el SDK de AWS con tus credenciales de SW3
-        const s3 = new AWS.S3({
-            accessKeyId: process.env.SW3_ACCESS_KEY_ID,
+
+        AWS.config.update({
+            accessKeyId: process.env.SW3_ACCESS_KEY,
             secretAccessKey: process.env.SW3_ACCESS_SECRET_KEY
         });
+        const s3 = new AWS.S3({
+            params: { Bucket: process.env.SW3_BUCKET_NAME },
+        });
 
-
-        // Guarda el archivo en SW3
         const now = new Date();
         const fileName = `document-${now.getTime()}-${Math.floor(Math.random() * 1000)}.pdf`;
+
+
         const params = {
             Bucket: process.env.SW3_BUCKET_NAME,
             Key: fileName,
             Body: pdfBuffer,
-            ContentType: 'application/pdf',
         };
+
         await s3.upload(params).promise();
 
         const fileUrl = `https://${process.env.SW3_BUCKET_NAME}.s3.amazonaws.com/${fileName}`;
